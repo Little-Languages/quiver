af993721a466c5e9a1553508a028463d129dcda0
“chrisshank” <chris.shank.23@gmail.com>
2024-04-16 23:07:28 -0700

    extract about an abstract arrow class

---
+++ src/abstract-arrow.ts
@@ -0,0 +1,86 @@
+import { ReactiveElement, PropertyValues } from '@lit/reactive-element';
+import { property } from '@lit/reactive-element/decorators.js';
+import vizObserver, { Rect } from 'viz-observer';
+
+export type { Rect };
+
+export class AbstractArrow extends ReactiveElement {
+  static tagName = 'abstract-arrow';
+
+  static register() {
+    customElements.define(this.tagName, this);
+  }
+
+  /** A CSS selector for the source of the arrow. */
+  @property({ type: String, reflect: true }) source: string = '';
+  #sourceCleanup: (() => void) | null = null;
+  #sourceRect!: Rect;
+
+  /** A CSS selector for the target of the arrow. */
+  @property({ type: String, reflect: true }) target: string = '';
+  #targetCleanup: (() => void) | null = null;
+  #targetRect!: Rect;
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.#unobserveSource();
+    this.#unobserveTarget();
+  }
+
+  #observerSource() {
+    this.#unobserveSource();
+    const el = document.querySelector(this.source);
+
+    if (!el) {
+      throw new Error('source is not a valid element');
+    }
+    return vizObserver(el, (rect) => {
+      this.#sourceRect = rect;
+      this.requestUpdate();
+    });
+  }
+
+  #unobserveSource() {
+    this.#sourceCleanup?.();
+    this.#sourceCleanup = null;
+  }
+
+  #observerTarget() {
+    this.#unobserveTarget();
+    const el = document.querySelector(this.target);
+
+    if (!el) {
+      throw new Error('source is not a valid element');
+    }
+    return vizObserver(el, (rect) => {
+      this.#targetRect = rect;
+      this.requestUpdate();
+    });
+  }
+
+  #unobserveTarget() {
+    this.#targetCleanup?.();
+    this.#targetCleanup = null;
+  }
+
+  update(changedProperties: PropertyValues) {
+    super.update(changedProperties);
+
+    if (!this.source || !this.target) {
+      return;
+    }
+
+    if (changedProperties.has('source')) {
+      this.#sourceCleanup = this.#observerSource();
+    }
+
+    if (changedProperties.has('target')) {
+      this.#targetCleanup = this.#observerTarget();
+    }
+
+    this.render(this.#sourceRect, this.#targetRect);
+  }
+
+  // @ts-ignore
+  render(sourceRect: Rect, targetRect: Rect) {}
+}
