f13ef642bc52e7f2d831a0f42e44f32f01fc1dd5
“chrisshank” <chris.shank.23@gmail.com>
2024-04-17 01:30:38 -0700

    update demos

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { AbstractArrow } from './abstract-arrow.js';
-export type { Rect } from './abstract-arrow.js';
-
-export { PerfectArrow } from './perfect-arrow.js';
-export type { Arrow, ArrowType } from './perfect-arrow.js';
-
-export { XanaduArrow } from './xanadu-arrow.js';
