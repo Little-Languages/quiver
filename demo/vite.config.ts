9140fe0b5bcb3373f3b6dfd0a2dcc8716f9fa540
“chrisshank” <chris.shank.23@gmail.com>
2024-04-26 15:17:18 -0700

    update package.json and demo build

---
+++ demo/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'node:path';
+import { readdirSync } from 'node:fs';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: readdirSync(__dirname)
+        .filter((file) => file.endsWith('.html'))
+        .reduce((acc, file) => {
+          acc[file.replace('.html', '')] = resolve(__dirname, file);
+          return acc;
+        }, {} as Record<string, string>),
+    },
+    modulePreload: {
+      polyfill: false,
+    },
+  },
+});
