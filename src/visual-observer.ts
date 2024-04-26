dbe975030f7bf82896a7aa5c689e86181e0624fb
“chrisshank” <chris.shank.23@gmail.com>
2024-04-26 14:28:57 -0700

    remove counter

--- src/visual-observer.ts
+++ src/visual-observer.ts
@@ -3,7 +3,6 @@ import { VisualObserver, VisualObserverEntry } from 'viz-observer';
 type VisualObserverEntryCallback = (entry: VisualObserverEntry) => void;
 
 export class VisualObserverManager {
-  #elementCount = new WeakMap<Element, number>();
   #elementMap = new WeakMap<Element, Set<VisualObserverEntryCallback>>();
 
   #vo = new VisualObserver((entries) => {
@@ -22,23 +21,12 @@ export class VisualObserverManager {
     if (callbacks === undefined) {
       this.#vo.observe(target);
       this.#elementMap.set(target, (callbacks = new Set()));
-      this.#elementCount.set(target, 0);
     }
 
     callbacks.add(callback);
-    this.#elementCount.set(target, this.#elementCount.get(target)! + 1);
   }
 
   unobserve(target: Element, callback: VisualObserverEntryCallback): void {
-    const count = this.#elementCount.get(target);
-
-    if (count === undefined || count === 0) return;
-
-    if (count > 1) {
-      this.#elementCount.set(target, this.#elementCount.get(target)! - 1);
-      return;
-    }
-
     let callbacks = this.#elementMap.get(target);
 
     if (callbacks === undefined) return;
